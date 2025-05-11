4eaf8285ced7b6147492fabfcf2beb687f098faf
duck.__index <138041510+NormalDuck@users.noreply.github.com>
2025-05-11 16:39:47 -0700

    fix: use dictionary to seperate class contructor

--- src/feces/index.d.ts
+++ src/feces/index.d.ts
@@ -12,74 +12,76 @@ type deleteValue = {
 
 type PlayerObject = Player | Player[] | ((arg: Player) => boolean);
 
-export declare type changes = Map<
-  Component,
-  Map<
-    Player,
+declare namespace Feces {
+  type applyable = Map<
+    Component,
     { value: Map<Entity, any>; special: Map<Entity, deleteValue | nilValue> }
-  >
->;
+  > & { __d: Entity };
+  type deletes = Map<Player, Entity>;
+  type changes = Map<
+    Component,
+    Map<
+      Player,
+      { value: Map<Entity, any>; special: Map<Entity, deleteValue | nilValue> }
+    >
+  >;
+  function group(changes: changes, deletes?: deletes): Map<Player, applyable>;
+  class feces {
+    static group(
+      this: void,
+      changes: changes,
+      deletes?: deletes,
+    ): Map<Player, applyable>;
+    replicated: Entity;
+    queries: {
+      single: Query<[unknown]>;
+      pair: Query<[unknown]>;
+    };
+    world: World;
+    pendingChanges: changes;
+    pendingDeletes: deletes;
+    refs: { [S: string]: Entity };
 
-export declare type deletes = Map<Player, Entity>;
+    ref: (this: feces, key?: string) => Entity;
+    replicating: (
+      this: feces,
+      callback: (
+        component1: Component,
+        playerObj: PlayerObject,
+        entity: Entity,
+        any: any,
+        component2: Component,
+      ) => void,
+    ) => void;
 
-export declare type applyable = Map<
-  Component,
-  { value: Map<Entity, any>; special: Map<Entity, deleteValue | nilValue> }
-> & { __d: Entity };
+    apply: (this: feces, apply: applyable) => void;
+    delta: (this: feces) => LuaTuple<[changes, deletes]>;
 
-export declare function group(
-  changes: changes,
-  deletes?: deletes,
-): Map<Player, applyable>;
+    full: (this: feces) => applyable;
+    group: (changes: changes, deletes?: deletes) => Map<Player, applyable>;
 
-export class feces {
-  replicated: Entity;
-  queries: {
-    single: Query<[unknown]>;
-    pair: Query<[unknown]>;
-  };
-  world: World;
-  pendingChanges: changes;
-  pendingDeletes: deletes;
-  refs: { [S: string]: Entity };
-
-  ref: (this: feces, key?: string) => Entity;
-  replicating: (
-    this: feces,
-    callback: (
-      component1: Component,
-      playerObj: PlayerObject,
-      entity: Entity,
-      any: any,
-      component2: Component,
-    ) => void,
-  ) => void;
-
-  apply: (this: feces, apply: applyable) => void;
-  delta: (this: feces) => LuaTuple<[changes, deletes]>;
-
-  full: (this: feces) => applyable;
-  group: (changes: changes, deletes?: deletes) => Map<Player, applyable>;
-
-  added: (this: feces, callback: (entity: Entity) => void) => void;
-  changed: (
-    this: feces,
-    callback: (entity: Entity, component: Entity, value: any) => void,
-  ) => void;
-  removed: (
-    this: feces,
-    callback: (entity: Entity, component: Component, value: any) => void,
-  ) => void;
-  deleted: (this: feces, callback: (entity: Entity) => void) => void;
-
-  hooks: {
-    added: (callback: (entity: Entity) => void) => void;
+    added: (this: feces, callback: (entity: Entity) => void) => void;
     changed: (
+      this: feces,
       callback: (entity: Entity, component: Entity, value: any) => void,
     ) => void;
     removed: (
+      this: feces,
       callback: (entity: Entity, component: Component, value: any) => void,
     ) => void;
-    deleted: (entity: Entity, callback: (entity: Entity) => void) => void;
-  };
+    deleted: (this: feces, callback: (entity: Entity) => void) => void;
+
+    hooks: {
+      added: (callback: (entity: Entity) => void) => void;
+      changed: (
+        callback: (entity: Entity, component: Entity, value: any) => void,
+      ) => void;
+      removed: (
+        callback: (entity: Entity, component: Component, value: any) => void,
+      ) => void;
+      deleted: (entity: Entity, callback: (entity: Entity) => void) => void;
+    };
+  }
 }
+
+export = Feces;
